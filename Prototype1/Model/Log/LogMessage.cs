a9f377ee526455282ef403112dcda6b8a09c9d6a
Magnus Mølgaard Lund <mlund18@student.aau.dk>
2019-11-21 11:25:44 +0100

    Log update fixed

---
+++ Prototype1/Model/Log/LogMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Log {
+    public abstract class LogMessage {
+        public abstract DateTime Time { get; set; }
+        public abstract LogMessageType Type { get; }
+        public abstract string Message { get; set; }
+
+        protected readonly char _seperator = '|';
+
+        public abstract string GetSaveString();
+        public abstract string GetExportableString();
+        public abstract string GetMessageString();
+    }
+}
---
+++ Prototype1/Model/Log/LogSearchFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Log {
+    public class LogSearchFilter {
+
+        public LogSearchFilter(DateTime after, DateTime before, string userId, string locationId, string itemId) {
+            After = after;
+            Before = before;
+            UserId = userId;
+            LocationId = locationId;
+            ItemId = itemId;
+        }
+
+        public DateTime After { get; }
+        public DateTime Before { get; }
+        public string UserId { get; }
+        public string LocationId { get; }
+        public string ItemId { get; }
+
+    }
+}
