82ca60a2d3e1b7b36fd0a9e91bfa4b40e6c4c77e
Anders Sønderby <45236808+byggemand-bob@users.noreply.github.com>
2019-12-02 13:56:27 +0100

    add sql connector

---
+++ Prototype1/SQL_DB_test_Frame/SQL_DB_test_Frame/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Model;
+using Central_Controller;
+using Networking;
+
+namespace SQL_DB_test_Frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DB_connector connector = new DB_connector();
+            Stopwatch timer = new Stopwatch();
+            Sorting sorter = new Sorting();
+            Item item = new Item();
+            Location location = new Location();
+            Controller controller = new Controller();
+            Server server = new Server();
+            
+            timer.Start();
+            //string test_name, test2;
+
+            //console.writeline("give table string");
+            //test_name = console.readline();
+            //console.writeline("give value string");
+            //test2 = console.readline();
+            //connector.insert(test_name,test2);
+            //Console.WriteLine("Items: "+connector.Count());
+
+            //timer.Stop();
+            //Console.WriteLine("Time: " + timer.ElapsedMilliseconds);
+            //Console.ReadKey();
+
+
+            //Console.WriteLine("give value string");
+            //test_name = Console.ReadLine();
+            //Console.WriteLine("give table string");
+            //test2 = Console.ReadLine();
+            List<string>[] testList = new List<string>[3];
+            testList = connector.Select("*", "ps_cent_control_data");
+            //for (int index = 0; index < testList[2].Count - 1; index++)
+            //{
+            //    Console.WriteLine(testList[0][index] + " | " + testList[1][index] + " | " + testList[2][index]);
+            //    Console.ReadKey();
+            //}
+            List<string>[] testList2 = new List<string>[3];
+            testList2 = sorter.createCombinedList(testList);
+            //Console.WriteLine("Done1!");
+            //Console.ReadKey();
+            //Console.WriteLine("index: "+ testList2[2].Count);
+            //for (int index = 0; index < testList2[2].Count - 1; index++)
+            //{
+            //    Console.WriteLine(testList2[0][index] + " | " + testList2[1][index] + " | " + testList2[2][index]);
+            //}
+            //Console.WriteLine("Done2!");
+            //Console.ReadKey();
+            //string temp = "test";
+            //for (int index = 0; index < testList2[2].Count - 1; index++)
+            //{
+            //    Console.WriteLine(testList2[0][index] + " | " + testList2[2].Count);
+
+            //    if (testList2[0][index] == temp)
+            //    {
+            //        Console.ReadKey();
+            //    }
+            //    temp = testList2[0][index];
+            //}
+            //Console.WriteLine("Done3!");
+            //Console.ReadKey();
+
+            for (int i = 0; i < testList2[2].Count; i++)
+            {
+                controller.InitialAddItem(new Item(testList2[0][i]), sorter.locationStringToList(testList2[2][i]));
+            }
+
+            //Console.WriteLine("Count items: " + controller.UnPartitionedLocations["000A00"].Items.Count);
+            //Console.WriteLine("Count locations: " + controller.UnPartitionedLocations.Count);
+            //foreach (Location itemss in controller.UnPartitionedLocations.Values)
+            //{
+            //    Console.WriteLine("name locations: " + itemss.ID); 
+            //}
+            //int x = 0;
+            //foreach (string stringthing in testList2[2])
+            //{
+            //    if (stringthing != "000A00")
+            //    {
+            //        Console.WriteLine("fejl: " + stringthing + " | " + testList2[0][x]);
+            //    }
+            //    x++;
+            //}
+
+            controller.InitialPartitionUnpartitionedLocations();
+
+            Central_Controller.Client client = new Central_Controller.Client("01");
+
+            controller.AddClient(client);
+            Partition TestPartition = controller.NextPartition(client);
+
+            //Console.WriteLine("TestPartition Count: " + TestPartition.Locations[0].Items.Count);
+
+            server.StartServer();
+            server.SendPartition(TestPartition);
+
+
+
+            timer.Stop();
+            Console.WriteLine("Time: " + timer.ElapsedMilliseconds);
+            Console.WriteLine("Done4!");
+            Console.ReadKey();
+        }
+    }
+}
