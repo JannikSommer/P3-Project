384f1f2a2c55236b5cd18cd564f57e43203ad242
JannikSommer <43030017+JannikSommer@users.noreply.github.com>
2019-12-03 12:47:13 +0100

    sghsrtlhre

--- Prototype1/Prototype1/SAScanApp/MenuDataHandlerPage.xaml.cs
+++ Prototype1/Prototype1/SAScanApp/MenuDataHandlerPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 using Networking;
 
 namespace SAScanApp
@@ -36,6 +37,14 @@ namespace SAScanApp
             Client networkingClient = new Client();
             Central_Controller.Client DeviceClient = new Central_Controller.Client("Anders");
             (Model.Partition partition,  CommunicationHandler handler) = networkingClient.DownloadPartitionAsync(DeviceClient).Result;
+            if (handler != CommunicationHandler.Success)
+            {
+                DisplayAlert("Error", "An error occured", "Fix your shit!");
+            }
+            else
+            {
+                DisplayAlert(handler.ToString(), "No error", "You fixed your shit!");
+            }
         }
     }
 }
---
+++ Prototype1/ViewModel/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using Networking;
+using Central_Controller;
+using Model;
+using WPF_PC;
+
+namespace ViewModel
+{
+    public class Class1
+    {
+        public Thread NetworkingThread { get; set; }
+        public Controller Controller { get; set; }
+
+
+
+
+    }
+}
