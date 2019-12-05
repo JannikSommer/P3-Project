e6d3310f6d50644779d98cd1e0862778da26901e
Anders Sønderby <45236808+byggemand-bob@users.noreply.github.com>
2019-12-05 02:49:21 +0100

    Address1

--- Prototype1/SQL_DB_test_Frame/SQL_DB_test_Frame/Sorting.cs
+++ Prototype1/SQL_DB_test_Frame/SQL_DB_test_Frame/Sorting.cs
@@ -33,7 +33,7 @@ namespace SQL_DB_test_Frame
                     if (list[0][i] == combinedList[0][j])
                     {
                         combinedList[1][j] = (Int32.Parse(combinedList[1][j]) + Int32.Parse(list[1][i])).ToString();
-                        //combinedList[2][j] = /*locationCleaner(combinedList[2][j], list[2][i]);*/combinedList[2][j] + ";" + "000A00";//list[2][i];
+                        combinedList[2][j] = locationCombiner(combinedList[2][j], list[2][i]);
 
                         found = true;
                     }
@@ -42,10 +42,14 @@ namespace SQL_DB_test_Frame
                 {
                     combinedList[0].Add(list[0][i]);
                     combinedList[1].Add(list[1][i]);
-                    combinedList[2].Add("000A00"/*list[2][i]*/);
+                    combinedList[2].Add(locationChecker(list[2][i]));
                 }
             }
-
+            for (int i = 0; i < combinedList[2].Count; i++)
+            {
+                string LocationString = combinedList[2][i];
+                combinedList[2][i] = locationCleaner(LocationString);
+            }
             return combinedList;
         }
         private int findLargestList(List<string>[] list)
@@ -69,16 +73,97 @@ namespace SQL_DB_test_Frame
             }
             return locationList;
         }
-        //private void locationCleaner(string org, string add)
-        //{
-        //    List<string> originalLocations;
-        //    for (int i = 0; i < length; i++)
-        //    {
-        //        if (true)
-        //        {
-
-        //        } 
-        //    }
-        //}
+        private string locationCombiner(string org, string add)
+        {
+            org += ";" + locationChecker(add);
+            return org;
+        }
+        private string locationChecker(string location)
+        {
+            if (location == " ")
+            {
+                Console.WriteLine("FUCKUP1:" + location + "|");
+                Console.ReadKey();
+                return "999Z99";
+            }
+            if (location == "")
+            {
+                return "999Z99";
+            }
+            if (location == null)
+            {
+                Console.WriteLine("FUCKUP3:" + location + "|");
+                Console.ReadKey();
+                return "999Z99";
+            }
+            if (location.Length == 6)
+            {
+                return location;
+            }
+            if (location.Length < 6)
+            {
+                return "999Z99";
+            }
+            if (location.Length > 6)
+            {
+                bool semiFound = false;
+                for (int i = 0; i < location.Length; i++)
+                {
+                    if (location[i] == ';')
+                    {
+                        semiFound = true;
+                    }
+                }
+                if (semiFound)
+                {
+                    List<string> locationList = new List<string>();
+                    string Final = string.Empty;
+                    foreach (var difLocations in location.Split(';'))
+                    {
+                        locationList.Add(difLocations);
+                    }
+                    foreach (var difLocation in locationList)
+                    {
+                        Final = locationCombiner(Final,locationChecker(difLocation));
+                    } 
+                }
+                return "999Z99";
+            }
+            throw new Exception("Error! Unable to handle location:" + location);
+        }
+        private string locationCleaner(string locationString)
+        {
+            List<string> locationList = new List<string>();
+            List<string> returnLocationList = new List<string>();
+            foreach (var Location in locationString.Split(';'))
+            {
+                locationList.Add(Location);
+            }
+            string returnString = locationList[0];
+            bool dubFound = false;
+            int count;
+            for (int i = 1; i < locationList.Count; i++)
+            {
+                count = returnLocationList.Count;
+                for (int j = 0; j < count; j++)
+                {
+                    if (locationList[i] == returnLocationList[j])
+                    {
+                        dubFound = true;
+                        returnLocationList.Add(locationList[i]);
+                    }
+                }
+                if (!dubFound)
+                {
+                    returnLocationList.Add(locationList[i]);
+                    Console.WriteLine("Thing1: " + locationList[i]);
+                }
+            }
+            foreach (var location in returnLocationList)
+            {
+                returnString = locationCombiner(returnString, location); 
+            }
+            return returnString;
+        }
     }
 }
