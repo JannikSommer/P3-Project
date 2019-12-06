69b836225eefecd6cb67ddd547fc042ec5180fce
KristianWP <43957853+KristianWP@users.noreply.github.com>
2019-12-06 14:01:30 +0100

    UnitTesting

--- Prototype1/Unit_Tests/Controller_Tests/Controller_CheckPartition_Tests.cs
+++ Prototype1/Unit_Tests/Controller_Tests/Controller_CheckPartition_Tests.cs
@@ -9,7 +9,7 @@ namespace Unit_Tests.Controller_Tests
     public class Controller_CheckPartition_Tests
     {
         [TestMethod]
-        public void CheckCountedItem_Test1_SingleItemTest()
+        public void CheckPartition_Test1_SingleLocationTest()
         {
             // Arrange
             Controller TestController = new Controller();
@@ -35,7 +35,7 @@ namespace Unit_Tests.Controller_Tests
         }
 
         [TestMethod]
-        public void CheckCountedItem_Test2_SingleItemTest()
+        public void CheckPartition_Test2_SingleLocationTest()
         {
             // Arrange
             Controller TestController = new Controller();
@@ -61,7 +61,7 @@ namespace Unit_Tests.Controller_Tests
         }
 
         [TestMethod]
-        public void CheckCountedItem_Test3_SingleItemTest()
+        public void CheckPartition_Test3_SingleLocationTest()
         {
             // Arrange
             Controller TestController = new Controller();
@@ -177,6 +177,121 @@ namespace Unit_Tests.Controller_Tests
             // Assert
             Assert.AreEqual(Expected_VerifiedItems, TestController.ItemsVerified);
             Assert.AreEqual(Expected_PartitallyCountedItemsCount, TestController.PartiallyCountedItems.Count);
+        }
+
+        [TestMethod]
+        public void CheckPartition_Test7__MultiLocationItemTest_NotCounted()
+        {
+            // Arrange
+            Controller TestController = new Controller();
+
+            Item TestItem = new Item("001", "T-Shirt", "White", "Large");
+            TestItem.ServerQuantity = 10;
+            TestItem.CountedQuantity = -1;
+
+            Location TestLocation1 = new Location("001A01");
+            Location TestLocation2 = new Location("002A01");
+
+            TestItem.AddLocation(TestLocation1);
+            TestItem.AddLocation(TestLocation2);
+
+            Partition TestPartition = new Partition(true);
+            TestPartition.AddLocation(TestLocation1);
+            TestPartition.AddLocation(TestLocation2);
+
+            //Expected
+            int Expected_VerifiedItems = 0;
+            int Expected_PartitallyCountedItemsCount = 0;
+            int Expected_MultiLocationPartitions = 1;
+
+            // Act
+            TestController.CheckPartition(TestPartition);
+
+            // Assert
+            Assert.AreEqual(Expected_VerifiedItems, TestController.ItemsVerified);
+            Assert.AreEqual(Expected_PartitallyCountedItemsCount, TestController.PartiallyCountedItems.Count);
+            Assert.AreEqual(Expected_MultiLocationPartitions, TestController.MultiLocationPartitions[0].Count);
+        }
+
+        [TestMethod]
+        public void CheckPartition_Test8__MultiLocationItemTest_NotCounted()
+        {
+            // Arrange
+            Controller TestController = new Controller();
+
+            Item TestItem = new Item("001", "T-Shirt", "White", "Large");
+            TestItem.ServerQuantity = 10;
+            TestItem.CountedQuantity = -1;
+
+            Location TestLocation1 = new Location("001A01");
+            Location TestLocation2 = new Location("002A01");
+
+            TestItem.AddLocation(TestLocation1);
+            TestItem.AddLocation(TestLocation2);
+
+            Partition TestPartition = new Partition(true);
+            TestPartition.AddLocation(TestLocation1);
+
+            //Expected
+            int Expected_VerifiedItems = 0;
+            int Expected_PartitallyCountedItemsCount = 0;
+            int Expected_PriorityPartionsCount = 1;
+
+            // Act
+            TestController.CheckPartition(TestPartition);
+
+            // Assert
+            Assert.AreEqual(Expected_VerifiedItems, TestController.ItemsVerified);
+            Assert.AreEqual(Expected_PartitallyCountedItemsCount, TestController.PartiallyCountedItems.Count);
+            Assert.AreEqual(Expected_PriorityPartionsCount, TestController.PriorityPartitions.Count);
+        }
+
+        [TestMethod]
+        public void CheckPartition_Test9__MultiLocationItemTest_NotCounted()
+        {
+            // Arrange
+            Controller TestController = new Controller();
+
+            Item TestItem1 = new Item("001", "T-Shirt", "White", "Large");
+            TestItem1.ServerQuantity = 10;
+            TestItem1.CountedQuantity = -1;
+
+            Item TestItem2 = new Item("002", "Pants", "black", "Small");
+            TestItem2.ServerQuantity = 10;
+            TestItem2.CountedQuantity = -1;
+
+            Location TestLocation1 = new Location("001A01");
+            Location TestLocation2 = new Location("002A01");
+            Location TestLocation3 = new Location("001B01");
+            Location TestLocation4 = new Location("002B01");
+            Location TestLocation5 = new Location("003B01");
+
+            TestItem1.AddLocation(TestLocation1);
+            TestItem1.AddLocation(TestLocation2);
+
+            TestItem2.AddLocation(TestLocation3);
+            TestItem2.AddLocation(TestLocation4);
+            TestItem2.AddLocation(TestLocation5);
+
+            Partition TestPartition1 = new Partition(true);
+            TestPartition1.AddLocation(TestLocation1);
+
+            Partition TestPartition2 = new Partition(true);
+            TestPartition2.AddLocation(TestLocation3);
+            TestPartition2.AddLocation(TestLocation4);
+
+            //Expected
+            int Expected_VerifiedItems = 0;
+            int Expected_PartitallyCountedItemsCount = 0;
+            int Expected_PriorityPartionsCount = 1;
+
+            // Act
+            TestController.CheckPartition(TestPartition1);
+
+            // Assert
+            Assert.AreEqual(Expected_VerifiedItems, TestController.ItemsVerified);
+            Assert.AreEqual(Expected_PartitallyCountedItemsCount, TestController.PartiallyCountedItems.Count);
+            Assert.AreEqual(Expected_PriorityPartionsCount, TestController.PriorityPartitions.Count);
         }
     }
 }
